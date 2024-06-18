1a41806108c93dd081b300aec4f8d28565c367b1
skillyskele <nathan.kim.1440@gmail.com>
2024-06-18 09:20:31 -0400

    made more tests, still working on converting code to be compatible with SymbolArray

--- tests/test_sync_string.c
+++ tests/test_sync_string.c
@@ -97,12 +97,78 @@ void test_edit_distance_Simple(void) {
     SymbolArray *sa1 = createSymbolArray(s1, 3);
     SymbolArray *sa2 = createSymbolArray(s2, 5);
     // Call edit_distance
-    //TEST_ASSERT_EQUAL_INT(2, edit_distance(sa1, sa2));
+    TEST_ASSERT_EQUAL_INT(2, edit_distance(sa1, sa2));
 
+    // This is the improper way of freeing s1, s1 is only one pointer
     // for (int i = 0; i < j; i++) {
     //     free(s1[i]);
     // }
     free(s1); // if it ws malloc'd, then it must be freed!!!
+      
+    deleteSymbolArray(sa1);
+    deleteSymbolArray(sa2);
+
+}
+
+// NOTE: if s1 = {""}, then a memory leak occurs. No idea why.
+void test_edit_distance_EmptyString(void) {
+    char* s1[]  = {};
+    char* s2[] = {"a", "b", "c"};
+    SymbolArray *sa1 = createSymbolArray(s1, 0);
+    SymbolArray *sa2 = createSymbolArray(s2, 3);
+
+    TEST_ASSERT_EQUAL_INT(3, edit_distance(sa1, sa2));
+      
+    deleteSymbolArray(sa1);
+    deleteSymbolArray(sa2);
+
+}
+
+void test_edit_distance_TwoEmptyStrings(void) {
+    char* s1[]  = {};
+    char* s2[] = {};
+    SymbolArray *sa1 = createSymbolArray(s1, 0);
+    SymbolArray *sa2 = createSymbolArray(s2, 0);
+
+    TEST_ASSERT_EQUAL_INT(0, edit_distance(sa1, sa2));
+      
+    deleteSymbolArray(sa1);
+    deleteSymbolArray(sa2);
+
+}
+
+void test_edit_distance_SmallestStrings(void) {
+    char* s1[]  = {"j"};
+    char* s2[] = {"b"};
+    SymbolArray *sa1 = createSymbolArray(s1, 1);
+    SymbolArray *sa2 = createSymbolArray(s2, 1);
+
+    TEST_ASSERT_EQUAL_INT(2, edit_distance(sa1, sa2));
+      
+    deleteSymbolArray(sa1);
+    deleteSymbolArray(sa2);
+}
+
+void test_edit_distance_NoCommonSubsequence(void) {
+    char* s1[]  = {"a", "b", "c"};
+    char* s2[] = {"d", "e", "fg"};
+    SymbolArray *sa1 = createSymbolArray(s1, 3);
+    SymbolArray *sa2 = createSymbolArray(s2, 3);
+
+    TEST_ASSERT_EQUAL_INT(6, edit_distance(sa1, sa2));
+      
+    deleteSymbolArray(sa1);
+    deleteSymbolArray(sa2);
+
+}
+
+void test_edit_distance_IdenticalStrings(void) {
+    char* s1[]  = {"a", "b", "c"};
+    char* s2[] = {"a", "b", "c"};
+    SymbolArray *sa1 = createSymbolArray(s1, 3);
+    SymbolArray *sa2 = createSymbolArray(s2, 3);
+
+    TEST_ASSERT_EQUAL_INT(0, edit_distance(sa1, sa2));
       
     deleteSymbolArray(sa1);
     deleteSymbolArray(sa2);
@@ -119,7 +185,7 @@ int main(void) {
     // RUN_TEST(test_find_LCS_IdenticalStrings);
     // RUN_TEST(test_find_LCS_SubsequenceAtEnd);
 
-     RUN_TEST(test_edit_distance_Simple);
+    // RUN_TEST(test_edit_distance_Simple);
     // RUN_TEST(test_edit_distance_EmptyString);
     // RUN_TEST(test_edit_distance_TwoEmptyStrings);
     // RUN_TEST(test_edit_distance_SmallestStrings);
